2c60b1baa9cedbc0f43b1bdbacbd5c06ec883427
Eduardo Visa <edu.mvis29@outlook.com>
2022-05-31 20:35:47 -0500

    Se modificó el ejercicio1.html y (ejercicio2 - .html y .js)

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -1,5 +1,5 @@
-// Ejercicio #1
-// Escribir una función que tome un arreglo de números y devuelva el número mayor de la lista. Ejemplo del arreglo : [3, 5, 7, 1, 6]
+// Ejercicio #2
+// María llega a su entrevista de trabajo para el rol de Desarrollador Junior y se le pide que resuelva este problema: Dada una lista de números [-1, 3, 4, 2, 6], escribe una función en javascript para calcular el número más pequeño.
 
 const numMayor = (array) => {
     array.sort(function(a,b) {
